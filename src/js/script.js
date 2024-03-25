adcf85deb7faa50ee89e010b38d800ef5c80e756
Seryozha <syt.an@ya.ru>
2024-03-26 00:29:05 +0300

    Added about & statistics styles & tiny carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,16 @@
+import { tns } from 'tiny-slider/src/tiny-slider';
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('load');
+  tns({
+    container: '.about__carousel',
+    mode: 'carousel',
+    items: 5,
+    nav: false,
+    controls: false,
+    mouseDrag: true,
+    center: true,
+    loop: true,
+    fixedWidth: 280,
+    gutter: 32,
+  });
 });
